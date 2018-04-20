aedf7f132210b43690e563bd5cdfb242ef067b13
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-20 03:27:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+def countdown_with_sleep(number)
+  number = sleep(1)
+  return number
+end
 
-def countdown
+def countdown(number)
+  while number > 0
+    "#{number} SECOND(S)!"
+    number -= 1
+  end
 end

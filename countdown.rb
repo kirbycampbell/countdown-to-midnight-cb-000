ab13bddb1e12307c3acd233040a4895a6e8f721d
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-20 03:41:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,12 @@ def countdown(number)
    puts "#{number} SECOND(S)!"
     number -= 1
   end
+    "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
   while number > 0
     sleep(1)
-    num -= 1
+    num
   end
-  "HAPPY NEW YEAR!"
 end
